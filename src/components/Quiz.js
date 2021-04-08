b02469758cd13d6e7cf1e0c774d74f9219cbf8ec
mehmetsekeroglu <mehmetsekeroglu2019@gmail.com>
2021-04-08 22:25:17 +0200

    first commit

---
+++ src/components/Quiz.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import Question from "./Question"
+import AnswerList from "./AnswerList"
+import "./Quiz.css"
+
+export default function Quiz({questionList, step, skor, nextQuestion, checkAnswer}) {
+
+  return (
+      <div className="quiz">
+      <div className="table">
+        <p className="step">Frage: {step}</p>
+        <p className="skor"> {skor}</p>
+    </div>
+    <div><Question questions={questionList}/></div> 
+    <div><AnswerList questions={questionList} checkAnswer={checkAnswer}/></div> 
+    <button onClick={nextQuestion}>Nächste</button>
+    </div>
+  )
+}
